43a6ae8052cbc759c92d4987d67d4085c586078e
Nam Duong <nam.duong@arbin.com>
2025-01-15 17:03:02 +0700

    change cli name

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -3,9 +3,9 @@ import Button from "./button";
 
 export default function Card() {
   return (
-    <>
+    <div className="flex gap-2">
       <Button>Alert</Button>
       <Alert />
-    </>
+    </div>
   );
 }
