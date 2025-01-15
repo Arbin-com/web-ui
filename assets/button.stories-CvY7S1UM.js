ee24b8345be2751799b0ce00f6468c7b5340fe84
namduong-arbin <143731017+namduong-arbin@users.noreply.github.com>
2025-01-15 09:51:31 +0000

    Deploying to gh-pages from @ Arbin-com/web-ui@9373e5e4ba5b11d8ac171b66f25e8f4c32884e6d 🚀

---
+++ assets/button.stories-CvY7S1UM.js
@@ -0,0 +1,16 @@
+import{B as m}from"./button-_b_Mcq0V.js";import"./jsx-runtime-z8MfsBtr.js";import"./index-C9rmetsa.js";import"./index-BEn-qc9j.js";import"./index-33C-Po4A.js";import"./index-D-OUEn-9.js";const k={title:"Components/Button",component:m,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{type:{control:"select",options:["primary","default","dashed","link","text"]},size:{control:"select",options:["large","middle","small"]},color:{control:"select",options:["danger","primary","default","blue","cyan","gold","green","lime","magenta","orange","pink","purple","red","yellow","volcano","geekblue"]},variant:{control:"select",options:["dashed","link","text","outlined","solid","filled"]}}},e={args:{type:"primary",children:"Primary Button"}},r={args:{type:"default",children:"Default Button"}},t={args:{size:"large",children:"Large Button"}};var a,o,n;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    type: "primary",
+    children: "Primary Button"
+  }
+}`,...(n=(o=e.parameters)==null?void 0:o.docs)==null?void 0:n.source}}};var s,l,c;r.parameters={...r.parameters,docs:{...(s=r.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    type: "default",
+    children: "Default Button"
+  }
+}`,...(c=(l=r.parameters)==null?void 0:l.docs)==null?void 0:c.source}}};var i,p,d;t.parameters={...t.parameters,docs:{...(i=t.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    size: "large",
+    children: "Large Button"
+  }
+}`,...(d=(p=t.parameters)==null?void 0:p.docs)==null?void 0:d.source}}};const x=["Primary","Default","Large"];export{r as Default,t as Large,e as Primary,x as __namedExportsOrder,k as default};
